1b6554bdd767a6ca4b9e0b6ca99c16be0d2995e4
alivka-oko <alina.okotnikova2002@gmail.com>
2024-10-13 14:23:43 +0300

    refactor timer logic into a single function

--- Js-advanced/4-timer/app.js
+++ Js-advanced/4-timer/app.js
@@ -8,23 +8,24 @@ const page = {
   seconds: document.querySelector('[data-tag = "seconds"]'),
 };
 
-function renderDOMTimer() {
-  const now = new Date();
-  const daysInMonth = new Date(
-    now.getFullYear(),
-    now.getMonth() + 1,
-    0
-  ).getDate();
-  page.months.textContent = 12 - (now.getMonth() + 1);
-  page.days.textContent = daysInMonth - now.getDate();
-  page.hours.textContent = 23 - now.getHours();
-  page.minutes.textContent = 59 - now.getMinutes();
-  page.seconds.textContent = 59 - now.getSeconds();
-}
+function startTimer() {
+  const renderDOMTimer = () => {
+    const now = new Date();
+    const daysInMonth = new Date(
+      now.getFullYear(),
+      now.getMonth() + 1,
+      0
+    ).getDate();
+    page.months.textContent = 12 - (now.getMonth() + 1);
+    page.days.textContent = daysInMonth - now.getDate();
+    page.hours.textContent = 23 - now.getHours();
+    page.minutes.textContent = 59 - now.getMinutes();
+    page.seconds.textContent = 59 - now.getSeconds();
+  };
 
-// init
-(() => {
   setInterval(() => {
     renderDOMTimer();
   }, 1000);
-})();
+}
+
+startTimer();
